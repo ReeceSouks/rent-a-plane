faab9c5ddb81ae0c73350c32aad1c21784adb214
nabila siregar <nabilaputrirachmania@gmail.com>
2019-10-28 16:36:15 +0800

    routing

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,13 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: [ :show ] # I want to show the airplane listing/booking the user have when opening the user profile
+  resources :airplanes do
+    resources :bookings, only: [ :create, :destroy]
+  end
+  resources :bookings, only: [ :show ] do
+   resources :reviews, only: [ :create ]
+ end
+  # Reviews cannot be edited and will all be shown at the page when you want to reserve the plane with description, etc
+  # what if I want to have two separate reviews? one from the host one from the guest
 end
