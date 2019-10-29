e2777af3e178ee05bba58f1b58f7f18ecf2b453c
nabila siregar <nabilaputrirachmania@gmail.com>
2019-10-29 13:43:58 +0800

    edits on airplane controller and view

--- app/controllers/airplanes_controller.rb
+++ app/controllers/airplanes_controller.rb
@@ -34,6 +34,10 @@ class AirplanesController < ApplicationController
 
   private
 
+  def set_airplane
+    @airplane = Airplane.find(params[:id])
+  end
+
   def airplane_params
     params.require(:airplane).permit(:name)
   end
