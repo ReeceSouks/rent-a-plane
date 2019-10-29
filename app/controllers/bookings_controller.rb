cfb0f996cf7eeb09c39f8af8136cd3ff460140ec
Jielun Chen <chenjielunn@gmail.com>
2019-10-29 11:44:16 +0800

    added controllers

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,10 +1,23 @@
 class BookingsController < ApplicationController
   def show
+    @booking = Booking.find(params[:id])
   end
 
   def create
+    @booking = Booking.new(booking_params)
+    @booking.airplane = @airplane
+    @booking.airplane = Booking.find(params[:user_id][:airplane_id]) unless params[:user_id][:airplane_id].blank?
+    if @booking.save
+      redirect_to booking_path(@booking)
+    else
+      render :new
+    end
   end
 
   def destroy
+    @booking = Booking.find(params[:id])
+    @booking = @booking.airplane
+    @booking.destroy
+    redirect_to airplane_path(@airplane)
   end
 end
