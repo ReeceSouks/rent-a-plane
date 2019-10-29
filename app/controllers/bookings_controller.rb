12fbf284c12f9022ff00afa2b983d8ee7f3261ad
Jielun Chen <chenjielunn@gmail.com>
2019-10-29 15:50:21 +0800

    linking reserve button to user profile

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -11,8 +11,9 @@ class BookingsController < ApplicationController
     @booking.price = (@booking.end_date - @booking.start_date) * @airplane.price_per_day
     # @booking.airplane = Booking.find(params[:user_id][:airplane_id]) unless params[:user_id][:airplane_id].blank?
     if @booking.save
-      redirect_to booking_path(@booking)
+      redirect_to profile_path
     else
+      raise
       render 'airplanes/show'
     end
   end
