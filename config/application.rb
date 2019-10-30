71e75a5074d848da889e14e0dac8d17fca89c506
Jielun Chen <chenjielunn@gmail.com>
2019-10-31 00:07:44 +0800

    test

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,8 @@ module RentAPlane
           generate.assets false
           generate.helper false
           generate.test_framework  :test_unit, fixture: false
+          Rails::Application line
+          config.assets.paths << "#{Rails.root}/app/assets/videos"
         end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
@@ -22,3 +24,4 @@ module RentAPlane
     # the framework and any gems in your application.
   end
 end
+
