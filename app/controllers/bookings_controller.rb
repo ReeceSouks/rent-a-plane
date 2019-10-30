b37b87c050096cb54cf4ef504a8a042a67b7d102
nabila siregar <nabilaputrirachmania@gmail.com>
2019-10-30 12:50:37 +0800

    some changes in routes, views, controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,19 +1,37 @@
 class BookingsController < ApplicationController
+  def index
+    @bookings = Booking.all
+  end
+
   def show
     @booking = Booking.find(params[:id])
   end
 
   def create
-    @booking = Booking.new(booking_params)
-    @airplane = Airplane.find(params[:airplane_id])
-    @booking.airplane = @airplane
-    @booking.user = current_user
-    @booking.price = (@booking.end_date - @booking.start_date) * @airplane.price_per_day
+    if user_signed_in?
+      if current_user.present?
+        @profile = current_user
+        @airplane = Airplane.find(params[:airplane_id])
+        @booking = Booking.new(booking_params)
+        @booking.airplane = @airplane
+        @booking.user = current_user
+        @booking.price = (@booking.end_date - @booking.start_date) * @airplane.price_per_day
     # @booking.airplane = Booking.find(params[:user_id][:airplane_id]) unless params[:user_id][:airplane_id].blank?
-    if @booking.save
-      redirect_to profile_path
+        if @booking.save
+          redirect_to booking_path(@booking)
+          flash[:notice] = 'A trip has been booked!'
+        else
+          render 'airplanes/show'
+        end
+      else
+        flash[:alert] = 'Please log in before booking'
+        redirect_to new_user_session_path
+        # I want the user when cannot book because they haven't sign in, after they sign in
+        # they get redirected back to the booking page instead of profile page
+      end
     else
-      render 'airplanes/show'
+      flash[:alert] = 'Please log in before booking'
+      redirect_to new_user_session_path
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,4 @@
 Rails.application.routes.draw do
-  get 'reviews/create'
-  get 'bookings/show'
-  get 'bookings/create'
-  get 'bookings/destroy'
-  get 'airplanes/index'
-  get 'airplanes/show'
-  get 'airplanes/new'
-  get 'airplanes/create'
-  get 'airplanes/edit'
-  get 'airplanes/update'
-  get 'airplanes/destroy'
   get 'users/show'
   get 'pages/home'
   devise_for :users
@@ -20,7 +9,7 @@ Rails.application.routes.draw do
   resources :airplanes do
     resources :bookings, only: [ :create, :destroy]
   end
-  resources :bookings, only: [ :show ] do
+  resources :bookings, only: [ :index, :show ] do
    resources :reviews, only: [ :create ]
  end
   # Reviews cannot be edited and will all be shown at the page when you want to reserve the plane with description, etc
