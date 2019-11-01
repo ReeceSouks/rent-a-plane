026c6ff645e188698e6a9178368e4f799173ce90
nabila siregar <nabilaputrirachmania@gmail.com>
2019-11-01 17:13:52 +0800

    autofill

--- app/javascript/plugins/init_autocomplete.js
+++ app/javascript/plugins/init_autocomplete.js
@@ -1,7 +1,15 @@
 import places from 'places.js';
 
 const initAutocomplete = () => {
-  const addressInput = document.getElementById('search-airport2');
+  const addressInput2 = document.getElementById('search-airport2');
+  const addressInput = document.getElementById('search-airport');
+  if (addressInput2) {
+    places({
+      appId: "pl0EV0VR85VD",
+      apiKey: "bf28a31df3fd8abfda4464f0b4f80f94",
+      container: addressInput2
+    });
+  }
   if (addressInput) {
     places({
       appId: "pl0EV0VR85VD",
